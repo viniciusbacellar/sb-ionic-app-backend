f0df848d7c0186465ba59da49f8d07fa482f51fc
Vinícius Bacellar <dev_bacellar@outlook.com>
2022-03-28 12:27:01 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/vinicius/sbionic/domain/Categoria.java
+++ src/main/java/com/vinicius/sbionic/domain/Categoria.java
@@ -3,9 +3,19 @@ package com.vinicius.sbionic.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_categoria")
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
