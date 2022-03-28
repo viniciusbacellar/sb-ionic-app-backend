3102b7d104cfdb19f986422198ba04341c21a79f
Vinícius Bacellar <dev_bacellar@outlook.com>
2022-03-28 11:35:18 -0300

    Projeto criado

---
+++ src/main/java/com/vinicius/sbionic/SbIonicApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.sbionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbIonicApplication.class, args);
+	}
+
+}
