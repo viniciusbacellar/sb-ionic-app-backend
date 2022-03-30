6ac152a98ef491866b1631064bb550f68adf07fb
Vinícius Bacellar <dev_bacellar@outlook.com>
2022-03-30 12:04:15 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/vinicius/sbionic/config/Config.java
+++ src/main/java/com/vinicius/sbionic/config/Config.java
@@ -8,15 +8,20 @@ import org.springframework.context.annotation.Configuration;
 
 import com.vinicius.sbionic.domain.Categoria;
 import com.vinicius.sbionic.domain.Cidade;
+import com.vinicius.sbionic.domain.Cliente;
+import com.vinicius.sbionic.domain.Endereco;
 import com.vinicius.sbionic.domain.Estado;
 import com.vinicius.sbionic.domain.Produto;
+import com.vinicius.sbionic.domain.enums.TipoCliente;
 import com.vinicius.sbionic.repositories.CategoriaRepository;
 import com.vinicius.sbionic.repositories.CidadeRepository;
+import com.vinicius.sbionic.repositories.ClienteRepository;
+import com.vinicius.sbionic.repositories.EnderecoRepository;
 import com.vinicius.sbionic.repositories.EstadoRepository;
 import com.vinicius.sbionic.repositories.ProdutoRepository;
 
 @Configuration
-public class Config implements CommandLineRunner{
+public class Config implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository repoCat;
@@ -26,39 +31,54 @@ public class Config implements CommandLineRunner{
 	private CidadeRepository repoCid;
 	@Autowired
 	private EstadoRepository repoEst;
-	
+	@Autowired
+	private ClienteRepository repoCli;
+	@Autowired
+	private EnderecoRepository repoEnd;
+
 	@Override
 	public void run(String... args) throws Exception {
 
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
 
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+
 		repoCat.saveAll(Arrays.asList(cat1, cat2));
 		repoProd.saveAll(Arrays.asList(p1, p2, p3));
-		
+
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "Saõ Paulo");
-		
+
 		Cidade c1 = new Cidade(null, "Uberlândia", est1);
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
-		
+
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 
 		repoEst.saveAll(Arrays.asList(est1, est2));
 		repoCid.saveAll(Arrays.asList(c1, c2, c3));
+
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		repoCli.saveAll(Arrays.asList(cli1));
+		repoEnd.saveAll(Arrays.asList(e1, e2));
 		
 	}
 
---
+++ src/main/java/com/vinicius/sbionic/domain/Cliente.java
@@ -0,0 +1,124 @@
+package com.vinicius.sbionic.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.vinicius.sbionic.domain.enums.TipoCliente;
+
+@Entity
+@Table(name = "tb_cliente")
+public class Cliente implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String email;
+	private String cpfOuCnpj;
+	private Integer tipo;
+	
+	@OneToMany(mappedBy="cliente")
+	private List<Endereco> enderecos = new ArrayList<>();
+	
+	@ElementCollection
+	// Tabela auxiliar no banco de dados para guardar os telefones
+	@CollectionTable(name="TELEFONE")
+	// Telefone classe simples, utilizando Set pois não aceita dados repetidos
+	private Set<String> telefones = new HashSet<>();
+	
+	public Cliente() {
+	}
+
+	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.cpfOuCnpj = cpfOuCnpj;
+		this.tipo = tipo.getCod();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpfOuCnpj() {
+		return cpfOuCnpj;
+	}
+
+	public void setCpfOuCnpj(String cpfOuCnpj) {
+		this.cpfOuCnpj = cpfOuCnpj;
+	}
+
+	public TipoCliente getTipo() {
+		return TipoCliente.toEnum(tipo);
+	}
+
+	public void setTipo(TipoCliente tipo) {
+		this.tipo = tipo.getCod();
+	}
+
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+
+	public Set<String> getTelefones() {
+		return telefones;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
+	
+	
+	
+}
