fc49fcdcbc8d6c5da4f4dc45e035e3fca8be05dc
Vinícius Bacellar <dev_bacellar@outlook.com>
2022-04-03 10:59:35 -0300

    Listadno todas categorias com DTO

--- src/main/java/com/vinicius/sbionic/resources/CategoriaResource.java
+++ src/main/java/com/vinicius/sbionic/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.vinicius.sbionic.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vinicius.sbionic.domain.Categoria;
+import com.vinicius.sbionic.dto.CategoriaDTO;
 import com.vinicius.sbionic.services.CategoriaService;
 
 @RestController
@@ -26,6 +29,15 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.status(HttpStatus.OK).body(obj);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		// Converte uma lista de objetos para objetosDTO
+		List<CategoriaDTO> listDto = list.stream()
+				.map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.status(HttpStatus.OK).body(listDto);
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
--- src/main/java/com/vinicius/sbionic/services/CategoriaService.java
+++ src/main/java/com/vinicius/sbionic/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.vinicius.sbionic.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,10 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 	
 	public Categoria insert(Categoria obj) {
