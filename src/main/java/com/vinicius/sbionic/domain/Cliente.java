6b088d857adef903408847d4f1cf5e46df66f0f8
Vinícius Bacellar <dev_bacellar@outlook.com>
2022-04-07 11:16:03 -0300

    Ajustando e testando a delecao de Cliente

--- src/main/java/com/vinicius/sbionic/domain/Cliente.java
+++ src/main/java/com/vinicius/sbionic/domain/Cliente.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -34,7 +35,7 @@ public class Cliente implements Serializable{
 	
 	
 	// anotação que faz com que seja possivel visualizar a lista de endereço na requisição
-	@OneToMany(mappedBy="cliente")
+	@OneToMany(mappedBy="cliente", cascade = CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	@ElementCollection
