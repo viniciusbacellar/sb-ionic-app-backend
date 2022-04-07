a020be0cdfa7651b7cb1f845d0256e079268a6ba
Vinícius Bacellar <dev_bacellar@outlook.com>
2022-04-07 12:28:05 -0300

    Validacao customizada de CPF ou CNPJ na insercao de Cliente

--- src/main/java/com/vinicius/sbionic/dto/ClienteNewDTO.java
+++ src/main/java/com/vinicius/sbionic/dto/ClienteNewDTO.java
@@ -2,20 +2,43 @@ package com.vinicius.sbionic.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.vinicius.sbionic.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message="Preenchimento obrgatório")
+	@Email(message="Email inválido")
 	private String email;
+	
+	@NotEmpty(message="Preenchimento obrgatório")
+	
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@NotEmpty(message="Preenchimento obrgatório")
 	private String logradouro;
+	
+	@NotEmpty(message="Preenchimento obrgatório")
 	private String numero;
+	
 	private String complemento;
 	private String bairro;
+	
+	@NotEmpty(message="Preenchimento obrgatório")
 	private String cep;
 	
+	@NotEmpty(message="Preenchimento obrgatório")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
