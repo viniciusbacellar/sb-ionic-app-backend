3e142d63f4dddbf25b1f2ccf303d278f4f7ecfb2
Vinícius Bacellar <dev_bacellar@outlook.com>
2022-04-21 13:27:24 -0300

    Esqueci a senha

--- src/main/java/com/vinicius/sbionic/services/AbstractEmailService.java
+++ src/main/java/com/vinicius/sbionic/services/AbstractEmailService.java
@@ -13,6 +13,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.vinicius.sbionic.domain.Cliente;
 import com.vinicius.sbionic.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService{
@@ -42,6 +43,22 @@ public abstract class AbstractEmailService implements EmailService{
 		return sm;
 	}
 	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+		sendEmail(sm);
+	}
+	
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitação de nova senha");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova senha: " + newPass);
+		return sm;
+	}
+
 	// retorna o HTML da confirmacaoPedido e set a variavel obj na palavra pedido dentro do template
 	protected String htmlFromTemplatePedido(Pedido obj) {
 		Context context = new Context();
--- src/main/java/com/vinicius/sbionic/services/EmailService.java
+++ src/main/java/com/vinicius/sbionic/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.vinicius.sbionic.domain.Cliente;
 import com.vinicius.sbionic.domain.Pedido;
 
 public interface EmailService {
@@ -15,4 +16,6 @@ public interface EmailService {
 	// Versão de email HTML
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	void sendHtmlEmail(MimeMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
