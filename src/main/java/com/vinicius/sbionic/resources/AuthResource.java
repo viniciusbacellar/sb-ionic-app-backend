9da991c06e7ec812c5aa29c8d18e8a981a5fbe60
Vinícius Bacellar <dev_bacellar@outlook.com>
2022-04-21 12:47:13 -0300

    Refresh token

---
+++ src/main/java/com/vinicius/sbionic/resources/AuthResource.java
@@ -0,0 +1,29 @@
+package com.vinicius.sbionic.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vinicius.sbionic.security.JWTUtil;
+import com.vinicius.sbionic.security.UserSS;
+import com.vinicius.sbionic.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+}
