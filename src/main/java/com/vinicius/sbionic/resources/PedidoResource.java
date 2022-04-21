acf11c3bc8726c1daf365367d0d1efb24837c115
Vinícius Bacellar <dev_bacellar@outlook.com>
2022-04-21 12:21:11 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/vinicius/sbionic/resources/PedidoResource.java
+++ src/main/java/com/vinicius/sbionic/resources/PedidoResource.java
@@ -5,12 +5,14 @@ import java.net.URI;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -37,4 +39,17 @@ public class PedidoResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<Page<Pedido>> findPage(
+			// Anotação para fazer os parâmetros receberem o valor durante a requisição
+			// value = nome da variável / deafultValue = valor padrão caso a variavel fique sem valor
+			@RequestParam(value="page", defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue="24")Integer linesPerPage,
+			@RequestParam(value="orderBy", defaultValue="instante")String orderBy,
+			@RequestParam(value="direction", defaultValue="DESC")String direction) {
+		Page<Pedido> list = service.findPage(page, linesPerPage, orderBy, direction);
+		return ResponseEntity.status(HttpStatus.OK).body(list);
+	}
+
 }
--- src/main/java/com/vinicius/sbionic/services/PedidoService.java
+++ src/main/java/com/vinicius/sbionic/services/PedidoService.java
@@ -4,8 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.vinicius.sbionic.domain.Categoria;
+import com.vinicius.sbionic.domain.Cliente;
 import com.vinicius.sbionic.domain.ItemPedido;
 import com.vinicius.sbionic.domain.PagamentoComBoleto;
 import com.vinicius.sbionic.domain.Pedido;
@@ -13,38 +18,40 @@ import com.vinicius.sbionic.domain.enums.EstadoPagamento;
 import com.vinicius.sbionic.repositories.ItemPedidoRepository;
 import com.vinicius.sbionic.repositories.PagamentoRepository;
 import com.vinicius.sbionic.repositories.PedidoRepository;
+import com.vinicius.sbionic.security.UserSS;
+import com.vinicius.sbionic.services.exceptions.AuthorizationException;
 import com.vinicius.sbionic.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class PedidoService {
-	
+
 	@Autowired
 	private PedidoRepository repo;
-	
+
 	@Autowired
 	private BoletoService boletoService;
-	
+
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-	
+
 	@Autowired
 	private ProdutoService produtoService;
-	
+
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-	
+
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@Autowired
 	private EmailService emailService;
 
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
-	
+
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
@@ -57,7 +64,7 @@ public class PedidoService {
 		}
 		obj = repo.save(obj);
 		pagamentoRepository.save(obj.getPagamento());
-		for(ItemPedido ip : obj.getItens()) {
+		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
 			ip.setProduto(produtoService.find(ip.getProduto().getId()));
 			ip.setPreco(ip.getProduto().getPreco());
@@ -66,5 +73,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
