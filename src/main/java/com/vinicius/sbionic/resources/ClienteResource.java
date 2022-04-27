1b14c01a59cc893910b7c17c15c3740314c4c5a4
Vinícius Bacellar <dev_bacellar@outlook.com>
2022-04-27 11:04:27 -0300

    Endpoint para buscar Cliente por email

--- src/main/java/com/vinicius/sbionic/resources/ClienteResource.java
+++ src/main/java/com/vinicius/sbionic/resources/ClienteResource.java
@@ -38,6 +38,12 @@ public class ClienteResource {
 		return ResponseEntity.status(HttpStatus.OK).body(obj);
 	}
 	
+	@RequestMapping(value="/email", method=RequestMethod.GET)
+	public ResponseEntity<Cliente> find(@RequestParam(value="value") String email) {
+		Cliente obj = service.findByEmail(email);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<ClienteDTO>> findAll() {
